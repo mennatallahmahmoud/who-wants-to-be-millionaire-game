29d6a7df4e72c75654a9784ab4a02edbcb86342c
Mennat-Allah Mahmoud <mennamahamed3@gmail.com>
2025-02-22 22:37:29 +0200

    Fix Input Text Color

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -9,7 +9,7 @@ export default function Start({setUsername}) {
         <h1 className="text-white text-4xl font-bold italic">Who Wants to Be a Millionaire?</h1>
         <div className="flex justify-center items-center flex-col">
           <input type="text" onChange={(e) => setUserName(e.target.value)}
-          className="bg-white text-lg w-[50%] lg:w-[25%] border-2 border-b-gray-700 px-3 py-2 text-center rounded-md mb-3 focus-within:outline-0" placeholder="Enter Your Name" />
+          className="bg-white text-[#020230] text-lg w-[50%] lg:w-[25%] border-2 border-b-gray-700 px-3 py-2 text-center rounded-md mb-3 focus-within:outline-0" placeholder="Enter Your Name" />
           <button
           onClick={() => {
           setUsername(userName)
